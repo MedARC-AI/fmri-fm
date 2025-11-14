20f8ae5deff44a6352010ff035137ca03b9e40e4
Connor Lane <connor.lane858@gmail.com>
2025-11-14 17:40:50 +0000

    add old scripts for separate hcp/ukbb pretrain

---
+++ experiments/pretrain_hcp/launch_pretrain.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=8
+
+ROOT="${HOME}/fmri-fm"
+cd $ROOT
+
+set -a
+source .env
+set +a
+
+EXP_NAME="pretrain_hcp"
+EXP_DIR="experiments/${EXP_NAME}"
+
+name="${EXP_NAME}/01_n1800/pretrain"
+
+uv run torchrun --standalone --nproc_per_node=1 \
+    src/flat_mae/main_pretrain.py \
+    --cfg-path "${EXP_DIR}/pretrain.yaml" \
+    --overrides \
+    name="${name}" \
+    notes="pretrain on 1800 hcp shards; stream from r2" \
+    output_dir="${EXP_DIR}/checkpoints"
